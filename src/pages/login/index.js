f80db876a3881e614b1a54a87930d4f943307bc4
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-12 10:50:13 +0700

    adjust file name and directory, also add login function

---
+++ src/pages/login/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    localStorage.setItem("user", username);
+    navigate("/quiz");
+  };
+
+  return (
+    <div className="login-container">
+      <div className="card-container">
+        <h2>Login</h2>
+        <input
+          className="input-username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <div className="button-container">
+          <button className="login-button" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
