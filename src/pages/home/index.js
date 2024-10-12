f957d3a11b2af86581494e4671181b06c92170cc
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-12 10:49:47 +0700

    adjust file name and directory

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/home.css";
+import "../../styles/home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  const startQuiz = () => {
-    navigate("/quiz"); // Mengarahkan user ke halaman QuizPage
-  };
-
   return (
     <div className="home-container">
       <h1 className="head-title">Selamat datang di Quitest</h1>
-      <p className="body-home">
-        Quitest adalah aplikasi quiz sederhana yang dibuat menggunakan ReactJS.
-      </p>
+      <p>Quitest adalah aplikasi quiz sederhana yang dibuat menggunakan ReactJS.</p>
       <p>Pada aplikasi website ini, kita bakal main tebak-tebakan soal.</p>
-      <button className="login-button" onClick={startQuiz}>
+      <p>Sebelum main tebak-tebakan soal, kita login dulu ya.</p>
+      <button className="login-button" onClick={() => {navigate("/login")}}>
         Login
       </button>
     </div>
