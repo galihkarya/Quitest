f6918c0343fc95f4fd3d93dba070e9986db9cf9a
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-12 10:49:17 +0700

    adjust path directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home.js';
+import Home from './pages/home/';
+import Login from './pages/login/';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
