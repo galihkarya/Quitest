265f44e3667ddfb74dcc11c8c378e981b5ec81bc
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-12 12:19:44 +0700

    change the question type

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const fetchQuestions = async (amount = 5) => {
-  const response = await axios.get(`https://opentdb.com/api.php?amount=${amount}&category=27`);
+export const fetchQuestions = async () => {
+  const response = await axios.get(`https://opentdb.com/api.php?amount=5&category=27`);
   return response.data.results;
 };
