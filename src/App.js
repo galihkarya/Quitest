7e8bc4398e8d7f8fc33d8846a892749fdc8f4acc
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-12 11:34:51 +0700

    add quiz page to routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/';
 import Login from './pages/login/';
+import Quiz from './pages/quiz';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
   );
