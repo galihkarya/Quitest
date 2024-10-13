a4a77f897c02e5a203c5e3ca9029309449e7162b
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-13 08:36:49 +0700

    add result page

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 import "../../styles/result.css";
 
-function Result() {
-  return (
-    <div>result</div>
-  )
-}
-
-export default Result
+const Result = ({ score, totalQuestions }) => {
+    const navigate = useNavigate();
+  
+    const handleRestart = () => {
+      localStorage.removeItem('quizState');
+      navigate('/');
+    };
+  
+    return (
+      <div className='result-container'>
+        <h2>Quiz Result</h2>
+        <p>Correct Answers: {score}</p>
+        <p>Total Questions: {totalQuestions}</p>
+        <button onClick={handleRestart}>Go home</button>
+      </div>
+    );
+  };
+  
+  export default Result;
