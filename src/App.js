c6835fcea4587429ce79f2505c83fec12311187f
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-13 08:35:48 +0700

    add result page to route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/';
 import Login from './pages/login/';
 import Quiz from './pages/quiz';
+import Result from './pages/result';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
   );
