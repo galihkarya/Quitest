072dc3f43983c1491ff76a0aae86e68c55a1fdcb
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-10-13 08:35:17 +0700

    Delete axios.js

--- src/api/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchQuestions = async () => {
-  const response = await axios.get(`https://opentdb.com/api.php?amount=5&category=27`);
-  return response.data.results;
-};
